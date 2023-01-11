8f239c5fce8cc175fc5bc26e547a0d6090bd4b40
안기경 <dksrlrud@mz.co.kr>
2023-01-11 15:40:36 +0900

    fix:List, 단건 조회

--- spring-boot-backend/src/main/java/com/megazone/springbootbackend/controller/TestController.java
+++ spring-boot-backend/src/main/java/com/megazone/springbootbackend/controller/TestController.java
@@ -3,9 +3,14 @@ package com.megazone.springbootbackend.controller;
 import com.megazone.springbootbackend.model.TestUser;
 import com.megazone.springbootbackend.repository.TestRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/test")
@@ -20,4 +25,17 @@ public class TestController {
         repository.save(testUser);
         return testUser.getName();
     }
+
+    @GetMapping("/user")
+    public List<TestUser> selectTestName(){
+        final var userList = repository.findAll();
+        return userList;
+    }
+
+    @GetMapping("/one-user")
+    public Optional<TestUser> selectOneTestUser(Long id){
+        final var oneUser = repository.findById(id);
+        return oneUser;
+    }
+
 }
