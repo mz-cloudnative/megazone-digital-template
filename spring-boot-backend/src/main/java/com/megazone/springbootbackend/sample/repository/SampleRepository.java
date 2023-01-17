dfef8a66c12746ae8d761f557d81d188be28b181
Yunbok Oh <ohyunbok@mz.co.kr>
2023-01-17 10:31:22 +0900

    refactor: sample 정리

--- spring-boot-backend/src/main/java/com/megazone/springbootbackend/sample/repository/SampleRepository.java
+++ spring-boot-backend/src/main/java/com/megazone/springbootbackend/sample/repository/SampleRepository.java
@@ -1,14 +1,10 @@
 package com.megazone.springbootbackend.sample.repository;
 
-import com.megazone.springbootbackend.sample.model.domain.SampleData;
 import com.megazone.springbootbackend.sample.model.entity.SampleJpaEntity;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SampleRepository extends JpaRepository<SampleJpaEntity, Long> {
 
-  SampleData findByName(@Param("name") String name);
 }
