1372b971be02ab0747eeaab32a32ffd349763b12
pyun-geunhyung <geunhyung@mz.co.kr>
2023-02-01 15:44:53 +0900

    - 임시 commit

---
+++ spring-boot-backend/src/test/java/com/megazone/springbootbackend/sample/ClubTest.java
@@ -0,0 +1,12 @@
+package com.megazone.springbootbackend.sample;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ClubTest {
+    @Test
+    @DisplayName("클럽 단일 조회")
+    void selectClub() {
+
+    }
+}
